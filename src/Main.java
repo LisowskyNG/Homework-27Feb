15d1eacf71130a8bc50288b55039ed0c1f7db73a
Valery Maltsev <lisowsky@yandex.ru>
2023-03-06 16:29:05 +0300

    Use getters and setters

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,28 @@ import Homework27Feb.Book;
 
 public class Main {
     public static void main(String[] args) {
-       Author boris = new Author("Борис", "Немцов");
-       Author valeriya = new Author("Валерия", "Новодворская");
+        Author valeriya = new Author("Валерия", "Новодворская");
+        Author boris = new Author("Борис", "Немцов");
 
-       Book proshanieSlav = new Book("Прощание славянки", valeriya, 2009);
-       Book putinItogi = new Book("Путин.Итоги.10 лет", boris, 2008);
+        Book proshanieSlav = new Book("Прощание славянки", valeriya, 2009);
+        Book putinItogi = new Book("Путин.Итоги.10 лет", boris, 2008);
 
+        System.out.println(boris.getName() +" "+ boris.getLastName());
+        System.out.println(putinItogi.getTitle());
+        System.out.println(putinItogi.getPublishedDate());
 
+        System.out.println();
 
+        System.out.println(valeriya.getName() +" "+ valeriya.getLastName());
+        System.out.println(proshanieSlav.getTitle());
+        System.out.println(proshanieSlav.getPublishedDate());
 
+        proshanieSlav.setPublishedDate(2015);
+
+        System.out.println();
+
+        System.out.println(valeriya.getName() +" "+ valeriya.getLastName());
+        System.out.println(proshanieSlav.getTitle());
+        System.out.println(proshanieSlav.getPublishedDate());
     }
 }
