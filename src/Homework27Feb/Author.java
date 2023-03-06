51e1c56ed40d73ae5f444de36a9d7daf5b0aaf5d
Valery Maltsev <lisowsky@yandex.ru>
2023-03-06 15:26:29 +0300

    Created two classes Book and Author

---
+++ src/Homework27Feb/Author.java
@@ -0,0 +1,14 @@
+package Homework27Feb;
+
+public class Author {
+    private String name;
+    private String lastName;
+
+    public Author (String name, String lastName,) {
+        this.name = name;
+        this.lastName = lastName;
+
+    }
+
+
+}
---
+++ src/Homework27Feb/Book.java
@@ -0,0 +1,14 @@
+package Homework27Feb;
+
+public class Book {
+    private String title;
+    private Author author;
+    private int publishedDate;
+
+    public Book (String title, Author author, int publishedDate) {
+        this.title = title;
+        this.author = author;
+        this.publishedDate - publishedDate;
+    }
+
+}
